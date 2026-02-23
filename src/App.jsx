446709453031cc70466c758c07de515246ee7950
raghab-pandit01 <raghab.pandit@w3developers.com.np>
2026-02-23 09:04:52 +0545

    Refactor App component to initialize game board and define home start positions

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import ludoBoard from '/ludoboard.webp'
-import './App.css'
-import { Pawn } from '../components/pawn'
 
-function App() {
+
+const App= ()=>{
+
+  const board= Array(15).fill().map(()=> Array(15).fill(null));
+  // 1st index. Column,
+  //  2nd index. Row
+
+  const colors={
+    blue:{colStart:0, colEnd:5, rowStart:0, rowEnd:5},
+    red:{colStart:9, colEnd:14, rowStart:0, rowEnd:5},
+    yellow:{colStart:0, colEnd:5, rowStart:9, rowEnd:14},
+    green:{colStart:9, colEnd:14, rowStart:9, rowEnd:14},
+}
+
+  const homeStartCors=[[2,1],[3,1],[2,3],[3,3]]
+
+  for(const color in colors){
+    const {colStart, colEnd, rowStart, rowEnd} = colors[color];
+    for(let i=colStart; i<=colEnd; i++){
+      for(let j= rowStart; j<=rowEnd; j++ ){
+          const hsRow= j- rowStart;
+          const hsCol= i- colStart;
+        if(homeStartCors.some(([x,y])=> x===hsCol && y===hsRow)){
+          board[i][j]={type:"homeStart", color}
+        }
+        else{
+        board[i][j]={type:"Home", color}
+        }
+      }
+    }
+  }
+
+  console.log(board)
+  
+
 
   return (
-    <div className='flex items-center justify-center h-screen p-8'>
-      <img src={ludoBoard}/>
-      <Pawn/>
-    </div>
+    <></>
   )
 }
 
